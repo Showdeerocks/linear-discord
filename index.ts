312614bbef4dc474c260c6241d2a88c671260e7b
filipvvv <f@filip.world>
2023-03-09 02:20:47 +0100

    Prettier

--- index.ts
+++ index.ts
@@ -5,39 +5,50 @@ const port = 8080;
 const webhook = Deno.env.get("DISCORD_WEBHOOK_URL");
 // Array of [<field to scan Linear webhook for>, <label for that field in a Discord message>]. Ordered from top to bottom.
 const labels = [
-	['title', 'Title'],
-	['description', 'Description'],
-	['body', 'Body'],
-	['priorityLabel', 'Priority'],
-]
+  ["title", "Title"],
+  ["description", "Description"],
+  ["body", "Body"],
+  ["priorityLabel", "Priority"],
+];
 
 const handler: Handler = async (request: Request): Promise<Response> => {
   const message = await request.json();
-	console.log('New request: ', JSON.stringify(message))
+  console.log("New request: ", JSON.stringify(message));
 
-	const fields = []
-	for(const [field, label] of labels)
-		if(message.data[field]) fields.push({name: label, value: message.data[field], inline: false})
+  const fields = [];
+  for (const [field, label] of labels)
+    if (message.data[field])
+      fields.push({ name: label, value: message.data[field], inline: false });
 
-	let body;
+  let body;
   try {
     if (webhook) {
       body = await fetch(webhook, {
         headers: { "Content-Type": "application/json" },
         method: "POST",
         body: JSON.stringify({
-          embeds: [{
-							color: 6021786,
+          embeds: [
+            {
+              color: 6021786,
               title: `${message.type} ${message.action}d on ${message.data.team.name}'s Linear`,
               fields,
-            }],
+            },
+          ],
         }),
-      }).then(res => res.text())
-			.then(json => `Sent message! Discord responded with: ` + JSON.stringify(json));
-		} else { throw new Error('Could not find webhook env var') }
+      })
+        .then((res) => res.text())
+        .then(
+          (json) =>
+            `Sent message! Discord responded with: ` + JSON.stringify(json)
+        );
+    } else {
+      throw new Error("Could not find webhook env var");
+    }
   } catch (e) {
     console.error(e);
-		return new Response(`Improperly formatted webhook request, ${e}`, { status: 400})
+    return new Response(`Improperly formatted webhook request, ${e}`, {
+      status: 400,
+    });
   }
 
   return new Response(body, { status: 200 });
